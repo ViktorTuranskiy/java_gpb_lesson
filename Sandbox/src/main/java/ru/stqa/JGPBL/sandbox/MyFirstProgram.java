34dac6c50a44832d9c5e3c200408f28440e0e33a
Viktor Turanskiy <cao2.c19@gmail.com>
2022-03-25 17:03:39 +0300

    ДЗ2

--- Sandbox/src/main/java/ru/stqa/JGPBL/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/JGPBL/sandbox/MyFirstProgram.java
@@ -3,6 +3,14 @@ package ru.stqa.JGPBL.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		Point p1 = new Point(3, 4);
+		Point p2 = new Point(7, 1);
+		System.out.println("расстояние между точкой А с координатами x=" + p1.x + "/" + "y=" + p1.y + " и точкой B с координатами x=" + p2.x + "/" + "y=" + p2.y + " = " + distance(p1,p2));
+		System.out.println("расстояние между точкой А с координатами x=" + p1.x + "/" + "y=" + p1.y + " и точкой B с координатами x=" + p2.x + "/" + "y=" + p2.y + " = " + p2.distance(p1,p2));
 	}
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+	}
+
 }
