e43c9da6730bed187e64e8be5ab5a34b9ab25a63
Viktor Turanskiy <cao2.c19@gmail.com>
2022-03-25 17:15:53 +0300

    корректировка

--- Sandbox/src/main/java/ru/stqa/JGPBL/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/JGPBL/sandbox/MyFirstProgram.java
@@ -6,7 +6,7 @@ public class MyFirstProgram {
 		Point p1 = new Point(3, 4);
 		Point p2 = new Point(7, 1);
 		System.out.println("расстояние между точкой А с координатами x=" + p1.x + "/" + "y=" + p1.y + " и точкой B с координатами x=" + p2.x + "/" + "y=" + p2.y + " = " + distance(p1,p2));
-		System.out.println("расстояние между точкой А с координатами x=" + p1.x + "/" + "y=" + p1.y + " и точкой B с координатами x=" + p2.x + "/" + "y=" + p2.y + " = " + p2.distance(p1,p2));
+		System.out.println("расстояние между точкой А с координатами x=" + p1.x + "/" + "y=" + p1.y + " и точкой B с координатами x=" + p2.x + "/" + "y=" + p2.y + " = " + Point.distance(p1,p2));
 	}
 
 	public static double distance(Point p1, Point p2) {
